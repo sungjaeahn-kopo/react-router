93bf9cb7c2e2ce8e3219158c526f6cc7348192e6
sungjaeahn-kopo <80259014+sungjaeahn-kopo@users.noreply.github.com>
2022-01-11 14:02:46 +0900

    History Object

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
+import HistorySample from "./HistorySample";
 import Home from "./Home";
 import Profiles from "./Profiles";
 
@@ -17,11 +18,15 @@ const App = () => {
         <li>
           <Link to="/profiles">프로필</Link>
         </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
       <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 };
